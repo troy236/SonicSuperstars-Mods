9ef92c092cf321c8a56c993c073e996c809bfe6f
troy236 <31560656+troy236@users.noreply.github.com>
2023-11-14 19:55:13 +0000

    AlwaysOnMusic: Prevents Pausing when entering background

--- AlwaysOnMusic/AlwaysOnMusicPlugin.cs
+++ AlwaysOnMusic/AlwaysOnMusicPlugin.cs
@@ -6,18 +6,25 @@ using Orion;
 namespace AlwaysOnMusic;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class AlwaysOnMusicPlugin : BasePlugin {
+
     public override void Load() {
         var harmony = new Harmony("Superstars.AlwaysOnMusic");
         var originalSoundManager_OnApplicationFocus = AccessTools.Method(typeof(SoundManager), "OnApplicationFocus");
+        var originalSysPauseManager_OnApplicationFocus = AccessTools.Method(typeof(SysPauseManager), "OnApplicationFocus");
         if (originalSoundManager_OnApplicationFocus == null) {
             Log.LogError("Failed to find SoundManager.OnApplicationFocus");
             return;
         }
-        var postSoundManager_OnApplicationFocus = AccessTools.Method(typeof(AlwaysOnMusicPlugin), "HookSoundManager_OnApplicationFocus");
-        harmony.Patch(originalSoundManager_OnApplicationFocus, prefix: new HarmonyMethod(postSoundManager_OnApplicationFocus));
+        if (originalSysPauseManager_OnApplicationFocus == null) {
+            Log.LogError("Failed to find SysPauseManager.OnApplicationFocus");
+            return;
+        }
+        var post_OnApplicationFocus = AccessTools.Method(typeof(AlwaysOnMusicPlugin), "Hook_OnApplicationFocus");
+        harmony.Patch(originalSoundManager_OnApplicationFocus, prefix: new HarmonyMethod(post_OnApplicationFocus));
+        harmony.Patch(originalSysPauseManager_OnApplicationFocus, prefix: new HarmonyMethod(post_OnApplicationFocus));
     }
 
-    public static bool HookSoundManager_OnApplicationFocus() {
+    public static bool Hook_OnApplicationFocus() {
         return false;
     }
 }
