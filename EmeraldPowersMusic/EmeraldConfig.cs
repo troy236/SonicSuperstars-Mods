9554b0def75688979a49a168bdd6db5f89f04ef0
troy236 <31560656+troy236@users.noreply.github.com>
2023-11-15 12:12:33 +0000

    EmeraldPowersMusic: Fixed Sound effects Added config file

---
+++ EmeraldPowersMusic/EmeraldConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+
+namespace EmeraldPowersMusic;
+public class EmeraldConfig {
+    public bool AvatarMusic { get; set; }
+    public bool BulletMusic { get; set; }
+    public bool VisionMusic { get; set; }
+    public bool WaterMusic { get; set; }
+    public bool IvyMusic { get; set; }
+    public bool SlowMusic { get; set; }
+    public bool ExtraMusic { get; set; }
+    public bool SuperMusic { get; set; }
+    public bool AvatarStartupSound { get; set; }
+    public bool BulletUsageSound { get; set; }
+    public bool IvyUsageSound { get; set; }
+    public bool SlowStartupSound { get; set; }
+    public bool VisionStartupSound { get; set; }
+
+    public static EmeraldConfig Load() {
+        try {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "EmeraldMusicConfig.txt");
+            if (!File.Exists(path)) return new EmeraldConfig();
+            return JsonSerializer.Deserialize<EmeraldConfig>(File.ReadAllBytes(path));
+        }
+        catch (Exception ex) {
+            Console.WriteLine("Failed to read EmeraldMusicConfig.txt");
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Using default values");
+        }
+        return new EmeraldConfig();
+    }
+}
--- EmeraldPowersMusic/EmeraldPowersMusicPlugin.cs
+++ EmeraldPowersMusic/EmeraldPowersMusicPlugin.cs
@@ -8,9 +8,11 @@ namespace EmeraldPowersMusic;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class EmeraldPowersMusicPlugin : BasePlugin {
     private static bool _blockChangeVolume = false;
+    private static EmeraldConfig _config;
 
     public override void Load() {
         var harmony = new Harmony("Superstars.EmeraldPowersMusic");
+        _config = EmeraldConfig.Load();
         var originalSoundManager_Play = AccessTools.Method(typeof(SoundManager), "Play", new[] { typeof(SoundSourceTypes), typeof(string), typeof(int), typeof(SoundSourceSettings) });
         var originalSoundManager_ChangeVolumeAll = AccessTools.Method(typeof(SoundManager), "ChangeVolumeAll");
         if (originalSoundManager_Play == null) {
@@ -27,47 +29,75 @@ public class EmeraldPowersMusicPlugin : BasePlugin {
         harmony.Patch(originalSoundManager_ChangeVolumeAll, prefix: new HarmonyMethod(postSoundManager_ChangeVolumeAll));
     }
 
-
     public static bool HookSoundManager_Play(SoundSourceTypes type, int cue) {
-        //Comment this region out to have the sound effects play
         #region Emerald Long Sound Effects
         if (type == SoundSourceTypes.Player) {
             switch (cue) {
-                case 30411: //Avatar
-                    return false;
-                case 30418: //Vision
-                    return false;
-                case 30422: //Ivy
-                    return false;
-                case 30424: //Slow
-                    return false;
+                case 30011: //Avatar Sonic
+                case 30111: //Avatar Tails
+                case 30211: //Avatar Knuckles
+                case 30311: //Avatar Amy
+                case 30411: //Avatar Trip
+                    return _config.AvatarStartupSound;
+                case 30009: //Bullet Sonic
+                case 30109: //Bullet Tails
+                case 30209: //Bullet Knuckles
+                case 30309: //Bullet Amy
+                case 30409: //Bullet Trip
+                    return _config.BulletUsageSound;
+                case 30018: //Vision Sonic
+                case 30118: //Vision Tails
+                case 30218: //Vision Knuckles
+                case 30318: //Vision Amy
+                case 30418: //Vision Trip
+                    return _config.VisionStartupSound;
+                case 30022: //Ivy Sonic
+                case 30122: //Ivy Tails
+                case 30222: //Ivy Knuckles
+                case 30322: //Ivy Amy
+                case 30422: //Ivy Trip
+                    return _config.IvyUsageSound;
+                case 30024: //Slow Sonic
+                case 30124: //Slow Tails
+                case 30224: //Slow Knuckles
+                case 30324: //Slow Amy
+                case 30424: //Slow Trip
+                    return _config.SlowStartupSound;
             }
         }
         #endregion
         if (type == SoundSourceTypes.BgmJingle) {
             switch (cue) {
                 case 90039: //Avatar
+                    if (_config.AvatarMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90038: //Bullet
+                    if (_config.BulletMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90041: //Vision
+                    if (_config.VisionMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90044: //Water
+                    if (_config.WaterMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90040: //Ivy
+                    if (_config.IvyMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90042: //Slow
+                    if (_config.SlowMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90043: //Extra
+                    if (_config.ExtraMusic) return true;
                     _blockChangeVolume = true;
                     return false;
                 case 90022: //Super
+                    if (_config.SuperMusic) return true;
                     _blockChangeVolume = true;
                     return false;
             }
