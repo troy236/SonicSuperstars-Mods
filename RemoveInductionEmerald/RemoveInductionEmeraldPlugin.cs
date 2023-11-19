1b3fbdd9531683842bf02d534e53a5cab360b0f3
troy236 <31560656+troy236@users.noreply.github.com>
2023-11-19 13:53:16 +0000

    Add RemoveInductionEmerald Mod

---
+++ RemoveInductionEmerald/RemoveInductionEmeraldPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using OriPlayer;
+
+namespace RemoveInductionEmerald;
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class RemoveInductionEmeraldPlugin : BasePlugin {
+    private static Harmony _harmony;
+
+    public override void Load() {
+        _harmony = new Harmony("Superstars.PressFactoryNoDrone");
+        var originalPlayerMain2D_UpDateFixedInductionEmerald = AccessTools.Method(typeof(PlayerMain2D), "UpDateFixedInductionEmerald");
+        if (originalPlayerMain2D_UpDateFixedInductionEmerald == null) {
+            Log.LogError("Failed to find PlayerMain2D.UpDateFixedInductionEmerald");
+            return;
+        }
+        var pre_UpDateFixedInductionEmerald = AccessTools.Method(typeof(RemoveInductionEmeraldPlugin), "Hook_UpDateFixedInductionEmerald");
+        _harmony.Patch(originalPlayerMain2D_UpDateFixedInductionEmerald, prefix: new HarmonyMethod(pre_UpDateFixedInductionEmerald));
+    }
+
+    public static bool Hook_UpDateFixedInductionEmerald() {
+        return false;
+    }
+}
