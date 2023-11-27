b498ef7f1a7f48ff0c3914680be8b7180dc241aa
troy236 <31560656+troy236@users.noreply.github.com>
2023-11-27 10:04:53 +0000

    Add KnucklesGlideSpeed Mod

---
+++ KnucklesGlideSpeed/KnucklesGlideSpeedConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace KnucklesGlideSpeed;
+public class KnucklesGlideSpeedConfig {
+    public float GlideSpeed { get; set; }
+    public bool UseStartXSpeed { get; set; }
+
+    public static KnucklesGlideSpeedConfig Load(BepInEx.Logging.ManualLogSource log) {
+        try {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "KnucklesGlideSpeedConfig.txt");
+            if (!File.Exists(path)) return GetDefault();
+            return JsonSerializer.Deserialize<KnucklesGlideSpeedConfig>(File.ReadAllBytes(path), new JsonSerializerOptions() { ReadCommentHandling = JsonCommentHandling.Skip });
+        }
+        catch (Exception ex) {
+            log.LogError("Failed to read KnucklesGlideSpeedConfig.txt");
+            log.LogError(ex.ToString());
+            log.LogError("Using default values");
+        }
+        return GetDefault();
+    }
+
+    private static KnucklesGlideSpeedConfig GetDefault() {
+        var knucklesGlideSpeedConfig = new KnucklesGlideSpeedConfig();
+        knucklesGlideSpeedConfig.GlideSpeed = 3f;
+        knucklesGlideSpeedConfig.UseStartXSpeed = true;
+        return knucklesGlideSpeedConfig;
+    }
+}
---
+++ KnucklesGlideSpeed/KnucklesGlideSpeedPlugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using OriPlayerAction;
+
+namespace KnucklesGlideSpeed;
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class KnucklesGlideSpeedPlugin : BasePlugin {
+    private static Harmony _harmony;
+    private static float _targetX;
+    private static KnucklesGlideSpeedConfig _config;
+
+    public override void Load() {
+        _harmony = new Harmony("Superstars.KnucklesGlideSpeed");
+        _config = KnucklesGlideSpeedConfig.Load(Log);
+
+        var originalPlActionJumpUniqueKnuckles_Init = AccessTools.Method(typeof(PlActionJumpUniqueKnuckles), "Init");
+        var originalPlActionJumpUniqueKnuckles_FixedUpdate = AccessTools.Method(typeof(PlActionJumpUniqueKnuckles), "FixedUpdate");
+        if (originalPlActionJumpUniqueKnuckles_Init == null) {
+            Log.LogError("Failed to find PlActionJumpUniqueKnuckles.Init");
+            return;
+        }
+        if (originalPlActionJumpUniqueKnuckles_FixedUpdate == null) {
+            Log.LogError("Failed to find PlActionJumpUniqueKnuckles.FixedUpdate");
+            return;
+        }
+        var pre_PlActionJumpUniqueKnuckles_Init = AccessTools.Method(typeof(KnucklesGlideSpeedPlugin), "Hook_PlActionJumpUniqueKnuckles_Init");
+        var post_PlActionJumpUniqueKnuckles_FixedUpdate = AccessTools.Method(typeof(KnucklesGlideSpeedPlugin), "Hook_PlActionJumpUniqueKnuckles_FixedUpdate");
+        _harmony.Patch(originalPlActionJumpUniqueKnuckles_Init, prefix: new HarmonyMethod(pre_PlActionJumpUniqueKnuckles_Init));
+        _harmony.Patch(originalPlActionJumpUniqueKnuckles_FixedUpdate, postfix: new HarmonyMethod(post_PlActionJumpUniqueKnuckles_FixedUpdate));
+    }
+
+    public static void Hook_PlActionJumpUniqueKnuckles_Init(PlActionJumpUniqueKnuckles __instance) {
+        if (_config.UseStartXSpeed) {
+            _targetX = System.Math.Max(3f, __instance.OwnerPlyer.VelocityX);
+        }
+        else {
+            _targetX = _config.GlideSpeed;
+        }
+    }
+
+    public static void Hook_PlActionJumpUniqueKnuckles_FixedUpdate(PlActionJumpUniqueKnuckles __instance) {
+        __instance.targetVelX = _targetX;
+    }
+}
